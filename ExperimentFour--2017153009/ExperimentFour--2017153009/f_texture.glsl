860255284fdd9e68d08fa038cd08cea2b9e042f0
wbt <2949588484@qq.com>
2020-06-14 23:13:45 +0800

    add four

---
+++ ExperimentFour--2017153009/ExperimentFour--2017153009/f_texture.glsl
@@ -0,0 +1,20 @@
+#version 330 core
+
+in vec4 color;
+in vec2 texCoord;
+in vec4 normal;
+in vec3 faceIndecies;
+
+out vec4 fColor;
+out vec4 fNormal;
+
+uniform sampler2D texture;
+
+void main()
+{
+
+    fColor = texture2D( texture, texCoord );
+
+    fNormal = normal;
+}
+
